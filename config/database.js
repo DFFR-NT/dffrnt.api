1eee130af57e0d98cb53dba015289304c647644c
Arian Johnson <leshaun.john@gmail.com>
2018-03-13 19:16:35 -0600

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,16 +1,16 @@
 
 module.exports = {
 	Config: {
-		user: 				'ajohnson',
-		database: 			'evectr',
+		user: 				'',
+		database: 			'',
 		connectionLimit: 	100,
 		multipleStatements: false,
 		debug: 				false,
 	},
 	Pool: {
 		HeadEx1: {
-			host: '18.216.8.221',
-			password: 'W{)=cMM6m+tw'
+			host: '',
+			password: ''
 		},
 	}
 };
