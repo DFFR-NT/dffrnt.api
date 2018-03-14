e2be20a7f7f6ba94e529f6e2978b1da90656dc39
Arian Johnson <leshaun.john@gmail.com>
2018-03-13 19:13:22 -0600

    Create npm_global.sh

---
+++ npm_global.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+npm install -g \
+	babel-cli \
+	bower \
+	component \
+	cors \
+	depcheck \
+	express \
+	grunt-cli \
+	gulp \
+	less \
+	less-plugin-clean-css \
+	n \
+	node-gyp \
+	nodemon \
+	nodetime \
+	npm;
