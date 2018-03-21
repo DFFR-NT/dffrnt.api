e028cf52a757647664e3cf8b699cd5e5bccf7881
Arian Johnson <leshaun.john@gmail.com>
2018-03-20 19:26:14 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 // Handle Requires ------------------------------------------------------------------------------
 
 	// All of our successive `require`s will be Babel'd
-	// require('babel-register')({
-		// presets: ['es2015'],
-		// ignore:  "node_modules"
-	// });
+	require('babel-register')({
+		presets: ['babel-preset-env'],
+		ignore:  /(node_modules\/)(?!dffrnt\.)[^\/]+(\/[^\/]+)*/
+	});
 
 	// Start the server
 	require('./main/server.js');
