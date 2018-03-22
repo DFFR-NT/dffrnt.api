4ef832b05d88dcb6d1b92cca56baade6c775d531
Arian Johnson <leshaun.john@gmail.com>
2018-03-22 01:58:01 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,13 @@
 	// All of our successive `require`s will be Babel'd
 	require('babel-register')({
 		presets: ['babel-preset-env'],
-		ignore:  /(node_modules\/)(?!dffrnt\.)[^\/]+(\/[^\/]+)*/
+		retainLines: true,
+		ignore: function ignore(filename) {
+			try {
+				var RGX = /(node_modules\/)(?!dffrnt\.)[^\/]+(\/[^\/]+)*/,
+					MCH = filename.match(RGX); return !!MCH;
+			} catch (e) { return true; }
+		}
 	});
 
 	// Start the server
