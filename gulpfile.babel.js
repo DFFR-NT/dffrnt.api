d3001138d327e8ff709bdeb26d9d6e15e501f1df
Arian Johnson <leshaun.john@gmail.com>
2018-04-01 15:54:32 -0600

    Add files via upload

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,11 +4,12 @@
 // ----------------------------------------------------------------------------------------------
 // Handle Requires ------------------------------------------------------------------------------
 
-	// Requires
+	// Requires ---------------------------------------------------------------------------------
 
 		import os 			from 'os';
 		import fs 			from 'fs';
 		import path 		from 'path';
+		import Assign 		from 'object-assign';
 		import gulp 		from 'gulp';
 		import nodemon 		from 'gulp-nodemon';
 		import babel 		from 'gulp-babel';
@@ -22,32 +23,81 @@
 		import map 			from 'map-stream';
 		import redis 		from 'redis';
 
-	// Configs
+	// Variables --------------------------------------------------------------------------------
 
-		const 	JSNS 	= (obj) => (JSON.stringify(obj, null, '    '));
-		const 	LOG 	= console.log;
+		const 	JSNS 	 = (obj) => (JSON.stringify(obj, null, '    '));
+		const 	LOG 	 = console.log;
+		const 	SERIES 	 = gulp.series;
+		const 	PARALLEL = gulp.parallel;
+		const 	WATCH 	 = (name, source, tasks) => {
+			return (done) => {
+				gulp.watch(source, PARALLEL(...tasks))
+					.on(  'change', (e) => { LOG(`${name} Change  |`, e); })
+					.on(   'error', (e) => { LOG(`${name} Error   |`, e); })
+					.on( 'nomatch', (e) => { LOG(`${name} NoMatch |`, e); });
+				done();
+			};
+		};
+
+		let 	crashes = null;
+
+	// Configs ----------------------------------------------------------------------------------
+
 		const 	cfgfld	= './config';
 		const 	pubFld 	= './public';
 		const 	brwFld 	= './main/browser';
 		const 	libFld 	= `${brwFld}/lib`;
 		const 	opsys 	= { linux: 'nix', darwin: 'nix', win32: 'win' }[os.platform()];
+		const 	frwk 	= {
+					folders: [ `./node_modules/dffrnt.*` ],
+					options: { cwd: `./` },
+					location: `./framework`
+				};
 		const 	conf 	= {
-					files: [ `./*.js` ],
-					options: { cwd: `${cfgfld}/.default` },
+					files: [ `./node_modules/dffrnt.confs/lib/*.cfg.js` ],
+					options: { cwd: `./` },
 					location: cfgfld
 				};
 		const 	brow 	= {
-					entries: [ `${brwFld}/main.js` ],
-					source: 'bundle.js', location: `${pubFld}/js`
+					source: 	'bundle.js',
+					options:  	{
+						entries: [ `${brwFld}/main.js` ],
+						debug: true,
+						cache: {},
+						packageCache: {},
+						fullPaths: false
+					},
+					minifyify:  {
+						map: false, uglify: {
+							mangle: true,
+							compress: {
+								sequences: true,
+								dead_code: true,
+								conditionals: true,
+								booleans: true,
+								unused: true,
+								if_return: true,
+								join_vars: true
+							}
+					} 	},
+					location: 	`${pubFld}/js`,
 				};
 		const 	jsx 	= {
 					source: `${libFld}/src/*.jsx`,
 					location: libFld,
 					presets: ['react','env']
 				};
-		const 	css 	= {
-					showLog: true, compatibility: 'ie8',
-					keepSpecialComments: 0, roundingPrecision: -1
+		const 	style 	= {
+					css: 	{ src: [
+						`${pubFld}/css/*.css`,
+						`!${pubFld}/css/*.min.css`
+					]	},
+					less: 	{ src: `${pubFld}/less/*.less` },
+					min: 	{
+						showLog: true, compatibility: 'ie8',
+						keepSpecialComments: 0, roundingPrecision: -1
+					},
+					location: `${pubFld}/css`
 				};
 		const 	redfl 	= {
 					cwd: '../REDIS',
@@ -67,174 +117,158 @@
 					}
 				}[opsys];
 
-	// Variables
-
-		let 	crashes = null;
-
-// ----------------------------------------------------------------------------------------------
-// Handle Functions -----------------------------------------------------------------------------
-
-		function runServer (done) {
-			let nmon = nodemon({ execMap: {
-							js: 'node --harmony',
-							tasks: ['demon','main'],
-						}})
-						.on('crash', () => {
-							try { LOG('Crashed ~ !!');
-								if ((new Date() - crashes) > 5000) {
-									nmon.emit('restart', 1);
-								}
-							} catch (e) { LOG(e); }
-						})
-						.on('restart', () => { LOG('Restarted ~ !!'); });
-			crashes = new Date(); nmon; done();
-		};
-
 // ----------------------------------------------------------------------------------------------
 // Handle Bundle Gulp ---------------------------------------------------------------------------
 
-	// Initialize Configs; if necessary
-		gulp.task( 'config', (done) => {
-			let max = 0;
-			gulp.src(conf.files, conf.options)
-				.pipe(map((file, done) => {
-					let len = file.basename.length;
-					max = (len>max?len:max);
-					done(null, file);
-				}))
-				.pipe(map((file, done) => {
-					let bse = file.base, nme = file.basename, fld = conf.location;
-					if (!fs.existsSync(`${bse}/../${nme}`)) {
-						LOG(`Copied default, [${nme.padStart(max)}], to ${fld} ...`);
-						gulp.src(file.path).pipe(gulp.dest(fld));
-					};
-					done(null, file);
-				}));
-			done();
-		});
+	// Configs ----------------------------------------------------------------------------------
 
-	// Browserify Concatenation
-		gulp.task( 'brow', (done) => {
-			function doBundle(watcher) {
-				watcher .bundle()
-						.pipe(source(brow.source))
-						// .pipe( babel({ presets: ['env'], compact: false }))
-						.pipe(gulp.dest(brow.location));
-				return 	watcher;
-			}
-
-			var bundler = new browserify({
-					entries: brow.entries,
-					debug: true,
-					cache: {},
-					packageCache: {},
-					fullPaths: false
-				})
-				.plugin('minifyify', {
-					map: false, uglify: {
-						mangle: true,
-						compress: {
-							sequences: true,
-							dead_code: true,
-							conditionals: true,
-							booleans: true,
-							unused: true,
-							if_return: true,
-							join_vars: true
-						}
-					},
-					compressPath: function (p) {
-						return path.relative(`${__dirname}/..`, p);
-					}
-				}),
-				watcher = watchify(bundler);
-
-			doBundle(watcher.on( 'update', () => {
-				var start = new Date(), src = brow.source,
-					frm1 = '[%s] Updating...', fin,
-					frm2 = '[%s] Updated: %ss';
-				LOG(frm1, src);
-				watcher = doBundle(watcher);
-				fin = ((new Date()-start)/1000);
-				LOG(frm2, src, fin.toFixed(3));
-				return watcher;
-			})); done();
-		});
-
-	//  JSX to  JS Conversion/Watch
-		gulp.task( 'jsx-make', (done) => {
-			gulp.src(jsx.source)
-				.pipe( babel({ presets: jsx.presets, compact: false }))
-				.on( 'error', (e) => { LOG('>>> ERROR', e); this.emit('end'); })
-				.pipe(gulp.dest(jsx.location));
-			done();
-		});
-		gulp.task( 'jsx-watch', (done) => {
-			gulp.watch(jsx.source, gulp.parallel('jsx-make'))
-				.on(  'change', (e) => { LOG('JSX Change: ', e); })
-				.on(   'error', (e) => { LOG('JSX Error:  ', e); })
-				.on( 'nomatch', (e) => { LOG('JSX NoMatch:', e); });
-			done();
-		});
-		gulp.task( 'jsx', gulp.parallel('jsx-make','jsx-watch'));
-
-	// LESS to CSS Conversion/Watch
-		// gulp.task( 'css-make', (done) => {
-			// gulp.src('./public/less/*.less')
-			// 	.pipe(less())
-			// 	.pipe(gulp.dest('./public/css'));
-			// done();
-		// });
-		// gulp.task('css-minify', (done) => {
-			// gulp.src('./public/css/*.css')
-			// 	// .pipe(sourcemaps.init({ debug: true }))
-			// 	.pipe(cssmin(css))
-			// 	.pipe(rename({ suffix: '.min' }))
-			// 	// .pipe(sourcemaps.write({ debug: true }))
-			// 	.pipe(gulp.dest('./public/css'));
-			// done();
-		// });
-		// gulp.task( 'css-convert', gulp.parallel('css-make','css-minify'));
-		// gulp.task( 'css-watch', (done) => {
-			// gulp.watch('./public/less/*.less', ['css-convert'])
-			// 	.on( 'change', (event) => {
-			// 		LOG('LESS Change:', event.path);
-			// 	})
-			// 	.on( 'error', (event) => {
-			// 		LOG('LESS Error:', 	event.path);
-			// 	})
-			// 	.on( 'nomatch', (event) => {
-			// 		LOG('LESS UnMatched:', event.path);
-			// 	}); done();
-		// });
-		// gulp.task( 'css', gulp.parallel('css-convert','css-watch'));
-
-	// REDIS Server Init
-		gulp.task('redis', (done) => {
-			let RED = redis.createClient();
-			RED.on('error', (err) => {
-				switch (err.code) {
-					// If it failed due to REDIS not running
-					case 'ECONNREFUSED':
-						exec([`cd ${redfl.cwd} &&`,rds.proc,rds.conf].join(' '), {
-							cwd: path.normalize(redfl.cwd)
-						}, (err, stdo, stde) => {
-							if (!!err) LOG(`REDIS.ERR: ${JSNS(err)}`);
-							else LOG(`REDIS.STD: ${JSNS({OUT:stdo,ERR:stde})}`);
-						});
-					// Not an issue, just need to know if it's UP
-					case 'NOAUTH': done(); break;
-					// Otherwise, log output; halt execution
-					default: LOG(JSNS(err));
-				}
+		// Initialize Configs; if necessary
+			gulp.task( 'config', (done) => {
+				let max = 0;
+				gulp.src(conf.files, conf.options)
+					.pipe(map((file, done) => {
+						let len = file.basename.length;
+						max = (len>max?len:max); done(null, file);
+					}))
+					.pipe(map((file, done) => {
+						let nme = file.basename, fld = conf.location;
+						if (!fs.existsSync(`${fld}/${nme}`)) {
+							LOG(`Copied default, [${nme.padStart(max)}], to ${fld} ...`);
+							gulp.src(file.path).pipe(gulp.dest(fld));
+						}; 	done(null, file);
+					})); 	done();
 			});
-			RED.on('ready', () => { LOG('REDIS is running'); done(); });
-		});
-		gulp.task( 'demon', runServer);
-		gulp.task( 'system', gulp.series('redis', 'demon'));
 
-	// Nodemon StartUp
-		gulp.task( 'main',   gulp.series('jsx'/*, 'css'*/, 'brow'));
-		gulp.task('default', gulp.series('config','system','main'));
+		// Initialize Framework Directory; if necessary
+			gulp.task( 'framework', (done) => {
+				let loc = frwk.location;
+				if (!fs.existsSync(loc)) fs.mkdirSync(loc), LOG(`Created Framework Directory.`);
+				gulp.src(frwk.folders, frwk.options)
+					.pipe(map((file, done) => {
+						let pth = file.path, nme = file.basename;
+						if (!fs.existsSync(`${loc}/${nme}`)) {
+							LOG(`Linking Module, [${nme}/], to ${loc} ...`);
+							gulp.src(pth).pipe(gulp.symlink(loc));
+						};	done(null, file);
+					})); 	done();
+			});
+
+		gulp.task('init', SERIES('config','convert','system'));
+
+	// Convert ----------------------------------------------------------------------------------
+
+		//  JSX to  JS Conversion/Watch
+			gulp.task('jsx-make',	(done) => {
+				gulp.src(jsx.source)
+					.pipe( babel({ presets: jsx.presets, compact: false }))
+					.on( 'error', (e) => { LOG('>>> ERROR', e); this.emit('end'); })
+					.pipe(gulp.dest(jsx.location));
+				done();
+			});
+			gulp.task('jsx-watch',	WATCH( 'JSX', jsx.source, ['jsx-make']));
+			gulp.task('jsx', 		PARALLEL('jsx-make','jsx-watch'));
+
+		// LESS to CSS Conversion/Watch
+			gulp.task('css-make',	(done) => {
+				gulp.src(style.less.src)
+					.pipe(less())
+					.pipe(gulp.dest(style.location));
+				done();
+			});
+			gulp.task('css-minify',	(done) => {
+				gulp.src(style.css.src)
+					// .pipe(sourcemaps.init({ debug: true }))
+					.pipe(cssmin(style.min))
+					.pipe(rename({ suffix: '.min' }))
+					// .pipe(sourcemaps.write({ debug: true }))
+					.pipe(gulp.dest(style.location));
+				done();
+			});
+			gulp.task('css-conv', 	SERIES('css-make','css-minify'));
+			gulp.task('css-watch',	WATCH('LESS', style.css.src, ['css-conv']));
+			gulp.task('css', 		PARALLEL('css-conv','css-watch'));
+
+		// Browserify Concatenation
+			gulp.task( 'brow', (done) => {
+				function doBundle(watcher) {
+					watcher .bundle()
+							.pipe(source(brow.source))
+							// .pipe( babel({ presets: ['env'], compact: false }))
+							.pipe(gulp.dest(brow.location));
+					return 	watcher;
+				}
+
+				var bundler = new browserify(brow.options)
+									.plugin('minifyify', Assign({
+										compressPath: p => path.relative(`${__dirname}/..`, p)
+									}, 	brow.minifyify)),
+					watcher = watchify(bundler);
+
+				doBundle(watcher.on( 'update', () => {
+					var start = new Date(), src = brow.source,
+						frm1 = '[%s] Updating...', fin,
+						frm2 = '[%s] Updated: %ss';
+					LOG(frm1, src);
+					watcher = doBundle(watcher);
+					fin = ((new Date()-start)/1000);
+					LOG(frm2, src, fin.toFixed(3));
+					return watcher;
+				})); done();
+			});
+
+		gulp.task('convert', SERIES('jsx', 'css', 'brow'));
+
+	// System -----------------------------------------------------------------------------------
+
+		// REDIS Server Init
+			gulp.task('redis', (done) => {
+				let RED = redis.createClient();
+				RED.on('error', (err) => {
+					switch (err.code) {
+						// If it failed due to REDIS not running
+						case 'ECONNREFUSED':
+							exec([`cd ${redfl.cwd} &&`,rds.proc,rds.conf].join(' '), {
+								cwd: path.normalize(redfl.cwd)
+							}, (err, stdo, stde) => {
+								if (!!err) LOG(`REDIS.ERR: ${JSNS(err)}`);
+								else LOG(`REDIS.STD: ${JSNS({OUT:stdo,ERR:stde})}`);
+							});
+						// Not an issue, just need to know if it's UP
+						case 'NOAUTH': done(); break;
+						// Otherwise, log output; halt execution
+						default: LOG(JSNS(err));
+					}
+				});
+				RED.on('ready', () => { LOG('REDIS is running'); done(); });
+			});
+
+		// Daemonized Startup
+			gulp.task('demon', (done) => {
+				let nmon = nodemon({ execMap: {
+								js: 'node --harmony',
+								tasks: ['demon','main'],
+							}})
+							.on('crash', () => {
+								try { LOG('Crashed ~ !!');
+									if ((new Date() - crashes) > 5000) {
+										nmon.emit('restart', 1);
+									}
+								} catch (e) { LOG(e); }
+							})
+							.on('restart', () => { LOG('Restarted ~ !!'); });
+				crashes = new Date(); nmon; done();
+			});
+
+		gulp.task( 'system', SERIES('redis', 'demon'));
+
+	// StartUp ----------------------------------------------------------------------------------
+
+		gulp.task('development', SERIES('init','convert','system'));
+		gulp.task('production',  SERIES('init','system'));
+
+		switch (process.env.NODE_ENV) {
+			case 'production': 	gulp.task('default', SERIES('production' ));
+			default: 			gulp.task('default', SERIES('development'));
+		}
 
 // ----------------------------------------------------------------------------------------------
