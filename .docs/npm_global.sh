c9b9016a62614f42f3e8164eee04d2af01099c77
Arian Johnson <leshaun.john@gmail.com>
2018-04-02 16:58:14 -0600

    Add files via upload

--- .docs/npm_global.sh
+++ .docs/npm_global.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 npm install -g \
-	babel-cli \
-	bower \
-	cors \
-	depcheck \
-	express \
-	gulp \
-	less \
-	less-plugin-clean-css \
-	nodemon;
+	babel-cli@6.26.0 \
+	bower@1.8.2 \
+	cors@2.8.4 \
+	depcheck@0.6.9 \
+	express@4.16.2 \
+	gulp@3.9.1 \
+	gulp-cli@2.0.1 \
+	less@3.0.1 \
+	less-plugin-clean-css@1.5.1 \
+	nodemon@1.17.1;
 
 exit $?;
