f69cc54ee79bbab74d47b65e8467ef8e064fd72c
Arian Johnson <leshaun.john@gmail.com>
2018-04-02 17:31:32 -0600

    Update npm_global.sh

--- .docs/npm_global.sh
+++ .docs/npm_global.sh
@@ -2,11 +2,12 @@
 
 npm install -g \
 	babel-cli@6.26.0 \
+	babel-core@6.7.4 \
 	bower@1.8.2 \
 	cors@2.8.4 \
 	depcheck@0.6.9 \
 	express@4.16.2 \
-	gulp@3.9.1 \
+	gulp@4.0.0 \
 	gulp-cli@2.0.1 \
 	less@3.0.1 \
 	less-plugin-clean-css@1.5.1 \
